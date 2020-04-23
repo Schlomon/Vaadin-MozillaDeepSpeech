422bba6766f4e85cfe56fcef872d01ea32599fb4
Schlomon <salo.bee@web.de>
2020-04-23 17:10:30 +0200

    Optimize import for MainView.java

--- VaadinProject/src/main/java/com/example/VaadinDeepSpeech/MainView.java
+++ VaadinProject/src/main/java/com/example/VaadinDeepSpeech/MainView.java
@@ -1,16 +1,12 @@
 package com.example.VaadinDeepSpeech;
 
-import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.server.PWA;
 
 /**
  * The main view contains two buttons and a speech recognizer
